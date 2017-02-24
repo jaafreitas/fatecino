612c2742817d297935d3b6add6a9e24bff5f1750
João Adriano Freitas <jaafreitas@gmail.com>
2017-02-24 01:45:10 -0300

    Projeto No. 2 – Potenciômetro http://www.fatecjd.edu.br/fatecino/arq_projetos/04-Projeto-2-Potenciometro.pdf Programa 2 – Controle da intensidade da luminosidade

--- Projeto02-Potenciometro/Projeto02.2-Potenciometro/Projeto02.2-Potenciometro.ino
+++ Projeto02-Potenciometro/Projeto02.2-Potenciometro/Projeto02.2-Potenciometro.ino
@@ -1,10 +1,10 @@
-// Liga e desliga um LED na frequência determinada pelo potenciômetro.
+// Controla a intensidade da luminosidade de um LED pela frequência determinada pelo potenciômetro.
 
 // Selecione o pino de entrada do potenciômetro.
 int pot = A0;
 
 // Selecione o pino do LED.
-int led = 13;
+int led = 9;
 
 // Variável para guardar o valor do potenciômetro.
 int valor = 0;
@@ -19,14 +19,12 @@ void setup() {
 void loop() {
   // Lê o valor do pontenciômetro em uma escala de 0 a 1023.
   valor = analogRead(pot);
-  
+
   // Mostra no monitor o valor obtido do potenciômetro.
   Serial.println(valor);
-  
-  digitalWrite(led, HIGH);
-  delay(valor);
-  
-  digitalWrite(led, LOW);
-  delay(valor);
+
+  // Acende o led com intensidade proporcional ao valor obtido
+  // em uma escala de 0 a 255
+  analogWrite(led, valor / 4);
 }
 
