487f35dce13598181ef8cd2438f397ea731b0bc6
João Adriano Freitas <jaafreitas@gmail.com>
2017-02-25 00:59:53 -0300

    Projeto No. 6 – LCD (Liquid Crystal Display 16x2)http://www.fatecjd.edu.br/fatecino/arq_projetos/09-Projeto-6-LCD.pdf 1. Exibição simples de texto 2. Rolagem (scroll) do texto

---
+++ Projeto06-LCD/Projeto06.1-LCD/Projeto06.1-LCD.ino
@@ -0,0 +1,33 @@
+// Exibição simples de texto
+
+#include <Adafruit_LiquidCrystal.h>
+
+// Os pinos para conexão do LCD podem variar de acordo com o modelo do Arduino.
+// SDA A4
+// SCL A5
+
+// Endereço encontrado utilizando o http://playground.arduino.cc/Main/I2cScanner
+Adafruit_LiquidCrystal lcd(0x20);
+
+void setup() {
+  // Inicia o LCD com dimensões 16x2 (Colunas x Linhas)
+  lcd.begin(16, 2);
+  lcd.print("Ola, FATECINO");
+}
+
+void loop() {
+  // Linha 1 e Coluna 0.
+  lcd.setCursor(0, 1);
+
+  // Escreve os segundos desde o Arduino foi ligado.
+  lcd.print(millis()/1000);
+
+  // Acende a luz de fundo.
+  lcd.setBacklight(HIGH);
+  delay(500);
+
+  // Apaga a luz de fundo.
+  lcd.setBacklight(LOW);
+  delay(500);
+}
+
---
+++ Projeto06-LCD/Projeto06.2-LCD/Projeto06.2-LCD.ino
@@ -0,0 +1,48 @@
+// Rolagem (scroll) do texto
+
+#include <Adafruit_LiquidCrystal.h>
+
+// Os pinos para conexão do LCD podem variar de acordo com o modelo do Arduino.
+// SDA A4
+// SCL A5
+
+// Endereço encontrado utilizando o http://playground.arduino.cc/Main/I2cScanner
+Adafruit_LiquidCrystal lcd(0x20);
+
+int inicio = 0;
+int tamanho = 1;
+boolean alterar = false;
+
+void setup() {
+  // Inicia o LCD com dimensões 16x2 (Colunas x Linhas)
+  lcd.begin(16, 2);
+}
+
+void loop() {
+  // Limpa o display LCD.
+  lcd.clear();
+  
+  String texto = "FATECINO - Clube de Arduino da Fatec";
+  if (tamanho < 16) {
+    // Posiciona o cursor na coordenada especificada
+    lcd.setCursor(16 - tamanho, 0);
+    lcd.print(texto.substring(inicio, tamanho));
+    tamanho++;
+  }
+  else {
+    if (!alterar) {
+      alterar = !alterar;
+      tamanho = 16;
+      lcd.setCursor(0, 0);
+    }
+    lcd.print(texto.substring(inicio, inicio + tamanho));
+    inicio++;
+  }
+  if (inicio > texto.length()) {
+    inicio = 0;
+    tamanho = 1;
+    alterar = !alterar;
+  }
+  delay(500);
+}
+
